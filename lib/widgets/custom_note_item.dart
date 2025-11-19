4e51073a97850ee77c064b50779a1bb0a24c903f
abdelwahabtest <abdelwahabelrashidy@gmail.com>
2025-11-19 15:53:02 +0200

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,57 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16,),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-               style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Build Your Career With Abdelwahab Emad',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete, color: Colors.black, size: 30),
               ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'Build Your Career With Abdelwahab Emad',
-                 style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),
-                  fontSize: 18,
+                'may21 , 2025',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                  fontSize: 16,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: (){},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 30,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'may21 , 2025',
-               style: TextStyle(
-                color: Colors.black.withOpacity(0.4),
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
